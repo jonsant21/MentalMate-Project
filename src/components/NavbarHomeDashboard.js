8a866356581e5aedfe730b56c9734f2b6f3db24c
Jose Olmos <jose.olmos.331@my.csun.edu>
2025-03-10 22:37:38 -0700

    Functionality to logout button on homedashboard

--- src/components/NavbarHomeDashboard.js
+++ src/components/NavbarHomeDashboard.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'; // Assuming you want to use the same styles
+import { useNavigate } from 'react-router-dom';
 
 function NavbarHomeDashboard() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('http://localhost:8081/logout', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include'
+      });
+
+      if (response.ok) {
+        // Redirect to home after successful logout
+        navigate('/');
+      } else {
+        // Handle logout error
+        console.error('Logout failed');
+      }
+    } catch (error) {
+      console.error('An error occurred while logging out:', error);
+    }
+  };
+
+
   return (
     <nav className="navbar">
       <h1>MentalMate</h1>
@@ -12,7 +39,7 @@ function NavbarHomeDashboard() {
         <li><Link to="/journaling">Journaling</Link></li>
         <li><Link to="/chat">Chat</Link></li>
         <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/">Logout</Link></li> {/* Just redirects to Home for now */}
+        <li><button onClick={handleLogout}>Logout</button></li> {/* Button for logout */}
       </ul>
     </nav>
   );
